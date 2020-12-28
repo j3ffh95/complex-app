a16834b551a53819b45e341c97f7be8e3e27b41d
j3ffh95 <j3ffh95@gmail.com>
2020-12-28 16:49:18 -0500

    Added and established a connection with our database

--- db.js
+++ db.js
@@ -1,1 +1,13 @@
 const mongodb = require("mongodb");
+
+const connectionString =
+  "mongodb+srv://j3ffh95:soccer1995@cluster0-ezsop.mongodb.net/ComplexApp?retryWrites=true&w=majority";
+
+mongodb.connect(
+  connectionString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, client) {
+    // This way if we require this file from within another file its going to return the database that we can work with
+    module.exports = client.db();
+  }
+);
