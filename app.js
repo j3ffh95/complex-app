523426a095996acae40a0c86122cb9e703a27a84
j3ffh95 <j3ffh95@gmail.com>
2020-12-27 22:17:23 -0500

    using ejs

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const app = express();
 
+// Set up app with the views folder, the first arg needs to be called views
+// the second one is the name of the folder you want your views to be in.
+// Now express knows where to look and find out our templates
+app.set("views", "views");
+
+// This code tells express which template engine we are using (In this project we are using ejs)
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  res.send("Welcome to our new app");
+  res;
 });
 
 app.listen(3000);
