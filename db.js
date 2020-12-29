074cd2ba474aff09644466ac09a043a5add3988e
j3ffh95 <j3ffh95@gmail.com>
2020-12-28 22:45:31 -0500

    added dotenv to create enviornmental variables

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const mongodb = require("mongodb");
 
-const connectionString =
-  "mongodb+srv://j3ffh95:soccer1995@cluster0-ezsop.mongodb.net/ComplexApp?retryWrites=true&w=majority";
-
 mongodb.connect(
-  connectionString,
+  process.env.CONNECTIONSTRING,
   { useNewUrlParser: true, useUnifiedTopology: true },
   function (err, client) {
     // This way if we require this file from within another file its going to return the database that we can work with
@@ -12,6 +12,6 @@ mongodb.connect(
 
     // Here we require our app server file once we established a connection with the database
     const app = require("./app");
-    app.listen(3000);
+    app.listen(process.env.PORT);
   }
 );
