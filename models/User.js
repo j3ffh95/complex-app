f65dcda29527c05cf826e51c2d91815ad6465fff
j3ffh95 <j3ffh95@gmail.com>
2021-01-11 22:42:23 -0500

    added a promise to the findone method from userscollection

--- models/User.js
+++ models/User.js
@@ -63,16 +63,18 @@ User.prototype.validate = function () {
 User.prototype.login = function () {
   return new Promise((resolve, reject) => {
     this.cleanUp();
-    usersCollection.findOne(
-      { username: this.data.username },
-      (err, attemptedUser) => {
+    usersCollection
+      .findOne({ username: this.data.username })
+      .then((attemptedUser) => {
         if (attemptedUser && attemptedUser.password == this.data.password) {
           resolve("congrats");
         } else {
           reject("invalid username and password");
         }
-      }
-    );
+      })
+      .catch(() => {
+        reject("Please try again later.");
+      });
   });
 };
 
