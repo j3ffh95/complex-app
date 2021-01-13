ec1d095d25d2f75611976a321d03a8c37a337c6f
j3ffh95 <j3ffh95@gmail.com>
2021-01-12 21:36:31 -0500

    Added functions to the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,18 @@
 const User = require("../models/User");
 
-exports.home = function (req, res) {
-  res.render("home-guest");
+exports.login = function (req, res) {
+  let user = new User(req.body);
+  user
+    .login()
+    .then(function (result) {
+      res.send(result);
+    })
+    .catch(function (error) {
+      res.send(error);
+    });
 };
+
+exports.logout = function (req, res) {};
 
 exports.register = function (req, res) {
   let user = new User(req.body);
@@ -14,14 +24,6 @@ exports.register = function (req, res) {
   }
 };
 
-exports.login = function (req, res) {
-  let user = new User(req.body);
-  user
-    .login()
-    .then(function (result) {
-      res.send(result);
-    })
-    .catch(function (error) {
-      res.send(error);
-    });
+exports.home = function (req, res) {
+  res.render("home-guest");
 };
