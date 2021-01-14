2823b226b77c64ba516bc3713d613bddadb0e138
j3ffh95 <j3ffh95@gmail.com>
2021-01-14 16:06:36 -0500

    took away the db method from the client

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ mongodb.connect(
   { useNewUrlParser: true, useUnifiedTopology: true },
   function (err, client) {
     // This way if we require this file from within another file its going to return the database that we can work with
-    module.exports = client.db();
+    module.exports = client;
 
     // Here we require our app server file once we established a connection with the database
     const app = require("./app");
