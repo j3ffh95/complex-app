8eda98f1816c818869ffdfd98e9bc735ac74440c
j3ffh95 <j3ffh95@gmail.com>
2021-01-14 16:20:29 -0500

    render the home view page

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,7 +28,7 @@ exports.register = function (req, res) {
 
 exports.home = function (req, res) {
   if (req.session.user) {
-    res.send("Welcome to the actual app");
+    res.render("home");
   } else {
     res.render("home-guest");
   }
