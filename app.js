556dfa461431eceb92c9bab21630575188a4bbf8
j3ffh95 <j3ffh95@gmail.com>
2021-01-23 15:15:13 -0500

    requiring the connect-flash package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
 const app = express();
 const router = require("./router");
 
