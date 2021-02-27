c38a557c8c41ab50e05a0f0c8371885f73c3c008
j3ffh95 <j3ffh95@gmail.com>
2021-02-27 12:37:35 -0500

    Created the create function for storing the user post to the database

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,5 @@
+const postsCollection = require("../db").db().collection("post");
+
 let Post = function (data) {
   this.data = data;
   this.errors = [];
@@ -27,6 +29,25 @@ Post.prototype.validate = function () {
     this.errors.push("You must provide a post content.");
   }
 };
-Post.prototype.create = function () {};
+Post.prototype.create = function () {
+  return new Promise((resolve, reject) => {
+    this.cleanUp();
+    this.validate();
+    if (!this.errors.length) {
+      // save post into database
+      postsCollection
+        .insertOne(this.data)
+        .then(() => {
+          resolve();
+        })
+        .catch(() => {
+          this.errors.push("Please try again later.");
+          reject(this.errors);
+        });
+    } else {
+      reject(this.errors);
+    }
+  });
+};
 
 module.exports = Post;
