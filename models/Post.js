9959885752ec25d0614e6d5192b832d711edaa62
j3ffh95 <j3ffh95@gmail.com>
2021-03-03 14:18:06 -0500

    Created the findSingleById function to get a valid id from mongo db

--- models/Post.js
+++ models/Post.js
@@ -32,6 +32,7 @@ Post.prototype.validate = function () {
     this.errors.push("You must provide a post content.");
   }
 };
+
 Post.prototype.create = function () {
   return new Promise((resolve, reject) => {
     this.cleanUp();
@@ -49,6 +50,21 @@ Post.prototype.create = function () {
         });
     } else {
       reject(this.errors);
+    }
+  });
+};
+
+Post.findSingleById = function (id) {
+  return new Promise(async function (resolve, reject) {
+    if (typeof id != "string" || !ObjectID.isValid(id)) {
+      reject();
+      return;
+    }
+    let post = await postCollection.findOne({ _id: new ObjectID(id) });
+    if (post) {
+      resolve(post);
+    } else {
+      reject();
     }
   });
 };
